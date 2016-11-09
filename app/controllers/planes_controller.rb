7609cf196ff2af597430e979e14d6d4e5574d49c
Alexandre <alexedron@gmail.com>
2016-11-09 17:05:28 +0100

    add navbar

--- app/controllers/planes_controller.rb
+++ app/controllers/planes_controller.rb
@@ -1,9 +1,13 @@
 class PlanesController < ApplicationController
-
+skip_before_action :authenticate_user!, only: [ :index, :show]
 before_action :find_plane, only: [:show]
   def index
+  if params[:query] && params[:query].length>0
+    @planes = Plane.where('description iLIKE ?', "%#{params[:query]}%")
+  else
     @planes = Plane.all
   end
+end
 
   def show
 
