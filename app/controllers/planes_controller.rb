ceddb40b7e232687c103f04e075026fbbb326242
Chris Appels <appelscj@gmail.com>
2016-11-10 12:02:53 +0100

    Implement bookings

--- app/controllers/planes_controller.rb
+++ app/controllers/planes_controller.rb
@@ -26,7 +26,7 @@ end
 
 
   def show
-
+    @booking = Booking.new
   end
 end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  get 'bookings/show'
+
+  get 'bookings/new'
+
+  get 'bookings/create'
+
   devise_for :users
   root to: 'pages#home'
 
